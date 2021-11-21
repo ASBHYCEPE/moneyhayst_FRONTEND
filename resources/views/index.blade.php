3b72d2e350c54f64f6c4c1d16e91fe807380b4fa
ASBHYCEPE <ashby.cepe@unc.edu.ph>
2021-11-21 14:43:59 +0800

    Added forms for input and css styling

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -1,9 +1,10 @@
 <!DOCTYPE html>
 <html>
     <head>
-        <title>SIDEBAR</title>
+        <title>MONEYHAYST</title>
         <meta charset="UTF-8">
         <link rel="stylesheet" href="{{asset('css/main.css')}}">
+        <link rel="stylesheet" href="{{asset('css/home.css')}}">
         <link rel="preconnect" href="https://fonts.googleapis.com">
         <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
         <link href="https://fonts.googleapis.com/css2?family=Work+Sans&display=swap" rel="stylesheet">
@@ -26,7 +27,60 @@
         </header>
 
         <div class="main-content">
-            
+            <div class="content-block">
+                <div class="content-item">
+                    <div class="content-title">
+                        <h4>INSERT TRANSACTION</h4>
+                    </div>
+                    <form id="main-form" action="">
+                        @csrf
+                        <div class="form-block">
+                            <div class="col-3">
+                                <label class="form-label">TRANSACTION TYPE</label>
+                            </div>
+                            <div class="col-4">
+                                <select name="transacType" id="transac-options" required>
+                                    <Option value="" disabled selected>SELECT TRANSACTION</Option>
+                                    <option value="0">INCOME</option>
+                                    <option value="1">EXPENSE</option>
+                                </select>
+                            </div>
+                        </div>
+                        <div class="form-block">
+                            <div class="col-3">
+                                <label class="form-label">CATEGORY</label>
+                            </div>
+                            <div class="col-4">
+                                <select name="category" id="category" required>
+                                    <Option value="" disabled selected>SELECT CATEGORY</Option>
+                                </select>
+                            </div>
+                        </div>
+                        <div class="form-block">
+                            <div class="col-3">
+                                <label class="form-label">TRANSACTION DATE</label>
+                            </div>
+                            <div class="col-4">
+                               <input type="date" name="transacDate" id="transac-date" required>
+                            </div>
+                        </div>
+                        <div class="form-block">
+                            <div class="col-3">
+                                <label class="form-label">TOTAL AMOUNT</label>
+                            </div>
+                            <div class="col-4">
+                               <input type="number" name="totalAmount" id="total-amount" required>
+                            </div>
+                        </div>
+                        <button type="submit">INSERT DETAILS</button>
+                    </form>
+                </div>
+                <div class="content-item">
+                    <div class="content-title">
+                        <h4>SUMMARY DETAILS</h4>
+                    </div>
+                </div>
+            </div>
         </div>
     </body>
 </html>
