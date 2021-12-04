e5bdd144ae1112cd749443e6409486dc33a6e79c
KimOliverManga3 <Kimoliver.manga@unc.edu.ph>
2021-12-05 01:30:59 +0800

    Finished the code for printing data and added the css

--- resources/views/expense.blade.php
+++ resources/views/expense.blade.php
@@ -1,17 +1,10 @@
-<?php
-    /*use App\Http\Controllers\navigationController;
-    if(isset($_POST["categories"])){
-        $val = $_POST["categories"];
-        expense($val);
-    }*/
-?>
-
 <!DOCTYPE html>
 <html>
     <head>
         <title>EXPENSE</title>
         <meta charset="UTF-8">
         <link rel="stylesheet" href="{{asset('css/main.css')}}">
+        <link rel="stylesheet" href="{{asset('css/expense.css')}}">
         <link rel="preconnect" href="https://fonts.googleapis.com">
         <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
         <link href="https://fonts.googleapis.com/css2?family=Work+Sans&display=swap" rel="stylesheet">
@@ -34,58 +27,56 @@
         </header>
 
         <div class="main-content">
-            
-            <div>
-                <form action = "/post" method = "POST">
-                    @csrf
-                    <select name="categories" id="category-type" required onchange = "this.form.submit()">
-                        <Option value="" disabled selected>Choose a Category</Option>
-                        <option value="all">Show All</option>
-                        <option value="food">Food</option>
-                        <option value="utilities">Utilities</option>
-                        <option value="comms">Communication</option>
-                        <option value="grocery">Grocery</option>
-                        <option value="meds">Medical</option>
-                        <option value="accessories">Accessories</option>
-                        <option value="transpo">Transportation</option>
-                        <option value="others">Others</option>
-                    </select>
-                </form>
-            </div>
 
-            <div>
+            <div class = "cell">
+                <div class="categories">
+                    <form action = "/postExpense" method = "POST">
+                        @csrf
+                        <select name="category" class= "selection" required onchange = "this.form.submit()">
+                            <Option value="" disabled selected>Choose a Category</Option>
+                            <option value="All">Show All</option>                 
+                            <option value="FOOD">Food</option>
+                            <option value="UTILITIES">Utilities</option>
+                            <option value="COMMUNICATION">Communication</option>
+                            <option value="GROCERY">Grocery</option>
+                            <option value="MEDICAL">Medical</option>
+                            <option value="ACCESSORIES">Accessories</option>
+                            <option value="TRANSPORTATION">Transportation</option>
+                            <option value="OTHERS">Others</option>
+                        </select>
+                    </form>
+                </div>
 
-                <table>
-
-                    <thead>
-                        <tr>
-                            <td>Category</td>
-                            <td>Transaction Date</td>
-                            <td>Amount</td>
-                        </tr>
-                    </thead>
-
-                    <tbody>
-                        @foreach ($transacData as $key => $data)
+                <div class = "printing">
+                    <table>
+                        <thead>
                             <tr>
-                                <td>{{$data->category}}</td>
-                                <td>{{$data->transac_date}}</td>
-                                <td>₱{{$data->amount}}</td>
+                                <td>Category</td>
+                                <td>Transaction Date</td>
+                                <td>Amount</td>
                             </tr>
-                        @endforeach
-                    </tbody>
+                        </thead>
 
-                </table>
+                        <tbody>
 
+                            @foreach ($transacData as $key => $data)
+                                <tr>
+                                    <td>{{$data->category}}</td>
+                                    <td>{{$data->transac_date}}</td>
+                                    <td>{{$data->amount}}</td>
+                                </tr>                        
+                            @endforeach
+                            
+                        </tbody>
+                    </table>
+                </div>
+
+                <div>
+                    <h3 class="expenses">Total Expenses: {{$totalExpense}}</h3>
+                </div>
             </div>
 
-            <div>
-
-                <h3>Total Expenses from this Category: </h3>
-                <h3>Total Expenses: </h3>
-
-            </div>
-
+            
         </div>
     </body>
 </html>
